0d79dd941d84d4c9297ca30964ba78cd7865edb0
Jamie / Null <bishjamie@icloud.com>
2016-07-02 17:10:24 +0100

    new UI, tabbed

--- BetaViewController.swift
+++ BetaViewController.swift
@@ -1,46 +1,43 @@
-/*
-    ViewController.swift
-    BetaFirmwares
- 
-    Created by Jamie Bishop on 29/06/2016.
-    Copyright © 2016 nullpixelLabs. All rights reserved.
- 
-    Thanks to reddit.com/u/AppleBetas & ipsw.me
-    Thanks also to the people who made SwiftyJSON, my favourite open-source JSON parsing framework.
-*/
+//
+//  BetaViewController.swift
+//  BetaFirmwares
+//
+//  Created by Jamie Bishop on 02/07/2016.
+//  Copyright © 2016 nullpixelLabs. All rights reserved.
+//
 
 import UIKit
 import SwiftyJSON
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class BetaViewController: UITableViewController {
+    
+    var betaFirmwares = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.parseJSON() //Parses all the JSON
-        //Will add pull to refresh soon.
+        self.parseJSON()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     
-    //I like to setup global variables here
-    var betaFirmwares = [String]()
-    @IBOutlet weak var tableView: UITableView!
-    
-    //MARK: Table View Population.
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.betaFirmwares.count;
-        /*
-            This function counts how many firmwares are in betaFirmwares array, and makes the table have that many rows.
-         */
-    }
-    
-    func tableView(tableView: UITableView,
+
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView,
                    cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.Default, reuseIdentifier:"cell")
         cell.textLabel?.text = betaFirmwares[indexPath.row]
         
         return cell
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.betaFirmwares.count;
     }
     
     //MARK: Parse JSON
@@ -64,4 +61,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         debugPrint(betaFirmwares)
     }
+
+   
 }
