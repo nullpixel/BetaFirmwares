d617968b3930009af75274e0ebd3266af87fee9c
BuildTools <applebetas@icloud.com>
2016-07-02 17:38:54 -0400

    Minor bugfixes - Add a debug mode for when you need to test but your internet sucks balls - Refresh control wasn't connected before, now it is (fixes #9) - Header view in detail view now sticks to top and scrolls with content (fixes #4) - Longer 30 second timeout for receiving JSON (for slower internet)

--- BetaFirmwares/BetaViewController.swift
+++ BetaFirmwares/BetaViewController.swift
@@ -11,6 +11,8 @@ import SwiftyJSON
 
 class BetaViewController: UITableViewController {
     
+    var debugMode = false
+    
     var betaFirmwares = [String: Firmware]()
     var hasAppeared = false
     
@@ -52,34 +54,40 @@ class BetaViewController: UITableViewController {
     
     // MARK: - Start receiving JSON
     func startReceive() {
-        DataReceiver.sharedInstance.getJSON() { json in
-            guard let json = json else {
-                let alert = UIAlertController(title: "Couldn't receive betas", message: "An error occurred while trying ot receive a list of the current betas on the OTA server.", preferredStyle: .Alert)
-                let action = UIAlertAction(title: "Try Again", style: .Cancel, handler: { _ in
-                    self.startReceive()
-                })
-                alert.addAction(action)
-                self.presentViewController(alert, animated: true, completion: nil)
-                return
-            }
-            self.betaFirmwares = [String: Firmware]()
-            for (deviceID, device) in json.dictionaryValue {
-                if let firmwares = device["firmwares"].array, deviceName = device["name"].string {
-                    for firmware in firmwares {
-                        if let version = firmware["version"].string, releaseType = firmware["releasetype"].string, buildNumber = firmware["buildid"].string where releaseType == "Beta" {
-                            let device = Device(name: deviceName, identifier: deviceID)
-                            if !self.betaFirmwares.keys.contains(version) {
-                                self.betaFirmwares[version] = Firmware(name: version, buildNumber: buildNumber, supportedDevices: [device])
-                            } else if !self.betaFirmwares[version]!.supportedDevices.contains(device) {
-                                self.betaFirmwares[version]!.supportedDevices.append(device)
+        if debugMode {
+            self.betaFirmwares = ["10.0": Firmware(name: "10.0", buildNumber: "M4d3Up", supportedDevices: [Device(name: "FakePhone 6", identifier: "FakePhone7,2")])]
+            self.tableView.reloadData()
+            self.refreshControl?.endRefreshing()
+        } else {
+            DataReceiver.sharedInstance.getJSON() { json in
+                guard let json = json else {
+                    let alert = UIAlertController(title: "Couldn't receive betas", message: "An error occurred while trying ot receive a list of the current betas on the OTA server.", preferredStyle: .Alert)
+                    let action = UIAlertAction(title: "Try Again", style: .Cancel, handler: { _ in
+                        self.startReceive()
+                    })
+                    alert.addAction(action)
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    return
+                }
+                self.betaFirmwares = [String: Firmware]()
+                for (deviceID, device) in json.dictionaryValue {
+                    if let firmwares = device["firmwares"].array, deviceName = device["name"].string {
+                        for firmware in firmwares {
+                            if let version = firmware["version"].string, releaseType = firmware["releasetype"].string, buildNumber = firmware["buildid"].string where releaseType == "Beta" {
+                                let device = Device(name: deviceName, identifier: deviceID)
+                                if !self.betaFirmwares.keys.contains(version) {
+                                    self.betaFirmwares[version] = Firmware(name: version, buildNumber: buildNumber, supportedDevices: [device])
+                                } else if !self.betaFirmwares[version]!.supportedDevices.contains(device) {
+                                    self.betaFirmwares[version]!.supportedDevices.append(device)
+                                }
                             }
                         }
                     }
                 }
-            }
-            dispatch_async(dispatch_get_main_queue()) {
-                self.tableView.reloadData()
-                self.refreshControl?.endRefreshing()
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.tableView.reloadData()
+                    self.refreshControl?.endRefreshing()
+                }
             }
         }
     }
